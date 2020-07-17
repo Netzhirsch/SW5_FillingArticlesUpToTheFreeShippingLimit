f96086b9afc545f834f9d0dea3b7457820e2bea4
luhmann <luhmann@netzhirsch.de>
2020-07-17 13:49:10 +0200

    FAUTFSL-1 Grundgerüst erstellen

---
+++ NetzhirschFillingArticlesUpToTheFreeShippingLimit.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace NetzhirschFillingArticlesUpToTheFreeShippingLimit;
+
+use Shopware\Components\Plugin;
+
+class NetzhirschFillingArticlesUpToTheFreeShippingLimit extends Plugin
+{
+
+}
